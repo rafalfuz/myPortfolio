f6a83622a0bbd1c3176bca9d349fbd15a71f78aa
Rafal Fuz <rafalfuz@gmail.com>
2022-07-14 23:16:09 +0200

    Added certyficate infos

--- src/views/Credentials/Credentials.js
+++ src/views/Credentials/Credentials.js
@@ -9,47 +9,47 @@ export const Credentials = () => {
       <h1 className="credentialsTitle">Credentials</h1>
       <div className="credentialsCardsWrapper">
         <div className="box">
+          {" "}
           <CredentialCard
-            title="Kurs podstawowy web-developper w 15 dni"
-            publisher="Udemy.com"
+            title="Web developer od podstaw w 15 dni"
+            publisher="Samuraj Programowania - Udemy"
             year="2020"
-            link="https://www.google.pl"
+            link="https://www.udemy.com/certificate/UC-7c3c0cf8-737a-46be-be42-fcaa619337a6"
           />
         </div>
         <div className="box">
           {" "}
           <CredentialCard
-            title="Kurs podstawowy web-developper w 15 dni"
-            publisher="Udemy.com"
+            title="Front-end zaawansowany w 15 dni"
+            publisher="Samuraj Programowania - Udemy"
             year="2020"
-            link="https://www.google.pl"
+            link="https://www.udemy.com/certificate/UC-ff3c1404-3192-4772-85cc-c15ea7c6da84"
+          />
+        </div>
+        <div className="box">
+          <CredentialCard
+            title="Programowanie w JavaScript"
+            publisher="Samuraj Programowania - Udemy"
+            year="2020"
+            link="https://www.udemy.com/certificate/UC-f8fb0cf6-24e0-4680-965d-3f2950c56f69/"
           />
         </div>
         <div className="box">
           {" "}
           <CredentialCard
-            title="Kurs podstawowy web-developper w 15 dni"
-            publisher="Udemy.com"
+            title="React od podstaw"
+            publisher="Samuraj Programowania - Udemy"
             year="2020"
-            link="https://www.google.pl"
+            link="https://www.udemy.com/certificate/UC-bc7be620-615a-4801-b254-569d787f5018"
           />
         </div>
         <div className="box">
           {" "}
           <CredentialCard
-            title="Kurs podstawowy web-developper w 15 dni"
-            publisher="Udemy.com"
+            title="Certyfikat React"
+            publisher="Adam Romański aka hello roman - eduweb.pl"
             year="2020"
-            link="https://www.google.pl"
-          />
-        </div>
-        <div className="box">
-          {" "}
-          <CredentialCard
-            title="Kurs podstawowy web-developper w 15 dni"
-            publisher="Udemy.com"
-            year="2020"
-            link="https://www.google.pl"
+            link="https://eduweb.pl/profil/133665/certyfikaty/8d22-c9bf-7be1-4cd6/react"
           />
         </div>
       </div>
@@ -58,3 +58,4 @@ export const Credentials = () => {
 };
 
 //https://codesandbox.io/s/github/pmndrs/react-spring/tree/master/demo/src/sandboxes/flip-card?file=/src/styles.module.css
+//ude.my/UC-bc7be620-615a-4801-b254-569d787f5018
